52d717e81d9759a03674d071d20369037dd5ee2e
Christos Giachoudis <giachoud@gapps.auth.gr.com>
2023-12-05 13:32:34 +0100

    bug4fix.c

--- bug4fix.c
+++ bug4fix.c
@@ -1,15 +1,13 @@
 /******************************************************************************
-* FILE: bug4.c
+* FILE: bug4fix.c
 * DESCRIPTION:
-*   This program demonstrates a condition variable race/synchronization 
-*   problem. It resembles the condvar.c program. One possible solution can
-*   be found in bug4fix.c
+*   This is just one way to resolve the synchronization problem demonstrated
+*   by bug4.c. A check is made in sub1 to make sure the pthread_cond_wait() 
+*   call is not made if the value of count is not what it expects. Its work is
+*   also placed after it is awakened, while count is locked.
 * SOURCE: 07/06/05 Blaise Barney
 * LAST REVISED: 01/29/09  Blaise Barney
 ******************************************************************************/
-
-// (for gcc include the -lm flag)
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,12 +15,11 @@
 #include <math.h>
 
 /* Define and scope what needs to be seen by everyone */
-#define NUM_THREADS 3
+#define NUM_THREADS  3
 #define ITERATIONS 10
 #define THRESHOLD 12
-
 int count = 0;
-double finalresult = 0.0;
+double finalresult=0.0;
 pthread_mutex_t count_mutex;
 pthread_cond_t count_condvar;
 
@@ -31,26 +28,33 @@ void *sub1(void *t)
 {
   int i; 
   long tid = (long)t;
-  double myresult = 0.0;
+  double myresult=0.0;
  
-  /* do some work */
-  sleep(1);
   /*
-    Lock mutex and wait for signal only if count is what is expected.  Note
-    that the pthread_cond_wait routine will automatically and atomically
-    unlock mutex while it waits. Also, note that if THRESHOLD is reached
-    before this routine is run by the waiting thread, the loop will be skipped
-    to prevent pthread_cond_wait from never returning, and that this thread's
-    work is now done within the mutex lock of count.
+  Lock mutex and wait for signal only if count is what is expected.  Note
+  that the pthread_cond_wait routine will automatically and atomically
+  unlock mutex while it waits. Also, note that if THRESHOLD is reached
+  before this routine is run by the waiting thread, the loop will be skipped
+  to prevent pthread_cond_wait from never returning, and that this thread's
+  work is now done within the mutex lock of count.
   */
   pthread_mutex_lock(&count_mutex);
-  printf("sub1: thread=%ld going into wait. count=%d\n", tid, count);
-  pthread_cond_wait(&count_condvar, &count_mutex);
-  printf("sub1: thread=%ld Condition variable signal received.", tid);
-  printf(" count=%d\n", count);
-  count++;
-  finalresult += myresult;
-  printf("sub1: thread=%ld count now equals=%d myresult=%e. Done.\n", tid, count, myresult);
+  if (count < THRESHOLD) {
+    printf("sub1: thread=%ld going into wait. count=%d\n",tid,count);
+    pthread_cond_wait(&count_condvar, &count_mutex);
+    printf("sub1: thread=%ld Condition variable signal received.",tid);
+    printf(" count=%d\n",count);
+    /* do some work */
+    sleep(1);
+    count++;
+    finalresult += myresult;
+    printf("sub1: thread=%ld count now equals=%d myresult=%e. Done.\n",
+           tid,count,myresult);
+    }
+  else {
+    printf("sub1: count=%d. Not as expected.",count); 
+    printf(" Probably missed signal. Skipping work and exiting.\n");
+    }
   pthread_mutex_unlock(&count_mutex);
   pthread_exit(NULL);
 }
@@ -89,8 +93,8 @@ void *sub2(void *t)
 
 int main(int argc, char *argv[])
 {
+  int i, rc; 
   long t1=1, t2=2, t3=3;
-  int i, rc;
   pthread_t threads[3];
   pthread_attr_t attr;
 
